09ef4bade8f8b7cc8b69e89a2a9f1217841e56f0
sudemiirtas <sudemiirtas@gmail.com>
2023-01-16 14:06:00 +0300

    basladık ya bismillah

--- proje.cpp
+++ proje.cpp
@@ -1,21 +1,22 @@
-#include <algorithm>   //algoritma kütüphanesi
-#include <chrono> //zaman kütühanesi
-#include <fstream> //dosya kütüphanesi
-#include <iostream> //giriş-çıkış kütüphanesi
-#include <queue>. //sınıf şablonları kütüphanesi
-#include <sstream> //ayrılmış dizi kütüphanesi
-#include <string> //dizi kütüphanesi
-#include <tuple>  //nesneleri tutan kütüphane
-#include <utility> //nesne çiftleri oluşturma kütüphane
-#include <vector> //vektör kütüphanesi
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <iostream>
+#include <algorithm>
+#include <sstream>
+#include <chrono>
+#include <vector>
 
-using DataType = int;   //veri tipi
-using DataStructure = std::queue<DataType>;
+class GPS {
+public:
+    double latitude;
+    double longitude;
+    std::string location_name;
+};
+ 
+std::chrono::high_resolution_clock::time_point start;
 
-//nanosaniyeye dönüştürme
-auto current_time()
-{
-  return std::chrono::duration_cast<std::chrono::nanoseconds>(
-             std::chrono::high_resolution_clock::now().time_since_epoch())
-      .count();
+void start_timer() {
+    // chrono kutuphanesi kullanarak zaman olcumunu icin saatin baslatilmasini gerceklestirir
+    start = std::chrono::high_resolution_clock::now();
 }
